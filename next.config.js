3a35c95322c8be04d1c845c0ddfd5d025166d6f9
L <Luen@users.noreply.github.com>
2024-01-12 16:46:03 +1000

    hopefully fix the miss items issue and prevent us redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     staticPageGenerationTimeout: 240,
+    i18n: {
+        locales: ['en-AU', 'en-US'],
+        defaultLocale: 'en-AU',
+        domains: [
+            {
+              domain: 'brewart.com',
+              defaultLocale: 'en-AU',
+            },
+            {
+              domain: 'brewart.com.au',
+              defaultLocale: 'en-AU',
+            },
+          ],
+    }
 }
 
 module.exports = nextConfig
