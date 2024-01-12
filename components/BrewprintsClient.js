89980d4aff3137218d935291ebf0303d15f94e9c
L <Luen@users.noreply.github.com>
2024-01-12 14:51:24 +1000

    add in more sorting

--- components/BrewprintsClient.js
+++ components/BrewprintsClient.js
@@ -8,6 +8,9 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
     const [isAlphabeticalAsc, setIsAlphabeticalAsc] = useState(true);
     const [isPriceAsc, setIsPriceAsc] = useState(true);
     const [isAlcoholVolumeAsc, setIsAlcoholVolumeAsc] = useState(true);
+    const [isBitternessAsc, setIsBitternessAsc] = useState(true);
+    const [isColourAsc, setIsColourAsc] = useState(true);
+    const [isBrewingTimeAsc, setIsBrewingTimeAsc] = useState(true);
 
     const sortAlphabetically = () => {
         const sorted = [...brewprints].sort((a, b) => {
@@ -18,7 +21,7 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
             }
         });
         setBrewprints(sorted);
-        setIsAlphabeticalAsc(!isAlphabeticalAsc); // Toggle the sort order for next click
+        setIsAlphabeticalAsc(!isAlphabeticalAsc);
     };
     
     const sortByPrice = () => {
@@ -30,7 +33,7 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
             }
         });
         setBrewprints(sorted);
-        setIsPriceAsc(!isPriceAsc); // Toggle the sort order for next click
+        setIsPriceAsc(!isPriceAsc);
     };    
 
     const sortByAlcoholVolume = () => {
@@ -40,11 +43,41 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
             return isAlcoholVolumeAsc ? volA - volB : volB - volA;
         });
         setBrewprints(sorted);
-        setIsAlcoholVolumeAsc(!isAlcoholVolumeAsc); // Toggle the sort order for next click
+        setIsAlcoholVolumeAsc(!isAlcoholVolumeAsc);
     };
 
+    const sortByBitterness = () => {
+        const sorted = [...brewprints].sort((a, b) => {
+            const bitterA = parseFloat(a.bitterness);
+            const bitterB = parseFloat(b.bitterness);
+            return isBitternessAsc ? bitterA - bitterB : bitterB - bitterA;
+        });
+        setBrewprints(sorted);
+        setIsBitternessAsc(!isBitternessAsc);
+    }
+
+    const sortByColour = () => {
+        const sorted = [...brewprints].sort((a, b) => {
+            const colourA = parseFloat(a.colour);
+            const colourB = parseFloat(b.colour);
+            return isColourAsc ? colourA - colourB : colourB - colourA;
+        });
+        setBrewprints(sorted);
+        setIsColourAsc(!isColourAsc);
+    }
+
+    const sortByBrewingTime = () => {
+        const sorted = [...brewprints].sort((a, b) => {
+            const timeA = parseFloat(a.approximateBrewingTime.split('-')[0]);
+            const timeB = parseFloat(b.approximateBrewingTime.split('-')[0]);
+            return isBrewingTimeAsc ? timeA - timeB : timeB - timeA;
+        });
+        setBrewprints(sorted);
+        setIsBrewingTimeAsc(!isBrewingTimeAsc);
+    }
+
     return (
-        <section className="mt-6 mx-auto max-w-2xl">
+        <section className="mt-6 mx-auto max-w-3xl">
             <h2 className="text-4xl font-bold dark:text-white/90">Brewprints</h2>
             <div className="flex justify-center gap-4 my-4">
                 <button onClick={sortAlphabetically} className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
@@ -56,6 +89,15 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
                 <button onClick={sortByAlcoholVolume} className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
                     {isAlcoholVolumeAsc ? 'Sort by Alcohol Volume (Asc)' : 'Sort by Alcohol Volume (Desc)'}
                 </button>
+                <button onClick={sortByBitterness} className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
+                    {isBitternessAsc ? 'Sort by Bitterness (Asc)' : 'Sort by Bitterness (Desc)'}
+                </button>
+                <button onClick={sortByColour} className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
+                    {isColourAsc ? 'Sort by Colour (Asc)' : 'Sort by Colour (Desc)'}
+                </button>
+                <button onClick={sortByBrewingTime} className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-lg shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
+                    {isBrewingTimeAsc ? 'Sort by Brewing Time (Asc)' : 'Sort by Brewing Time (Desc)'}
+                </button>
             </div>
             <div className="grid grid-cols-2 gap-4 mt-6">
                 {brewprints.map((item, index) => (
@@ -63,29 +105,26 @@ export default function BrewprintsClient({ initialBrewprints = [] }) {
                         <div>
                             <Link href={item.url}>
                                 <h3 className="text-xl font-bold dark:text-white/90">{item.name}</h3>
-                                <p className="mt-2 text-base dark:text-white/70">{item.shortDescription}</p>
+                                <p className="mt-2 text-base dark:text-white/70">{item.shortDescription.replace("-tsar","-star")}</p>
                                 <img src={item.imageBanner} alt={item.name} className="w-full h-auto mt-2" />
                                 <p className="mt-2 text-base dark:text-white/70">{item.longDescription.replace("PLEASE NOTE: This BrewPrint contains Dry Hops! The Dry Hop Filter is required to filter out hop debris.","").replace("** For a Limited Time only the Australian Lager BrewPrint comes with a BONUS Christmas gift wrap, so you can wrap Christmas up early! Pouring clean and clear with a bright golden hue, this refreshing brew brims with aromas of stone fruit and citrus. A dash of late hopping adds subtle tropical notes, perfect for sunny days and barbecues. Slow fermentation using traditional lager yeast brings delicate floral notes to the palate as the warm Aussie sun sinks below the horizon.&nbsp; See T&amp;Cs for full details. **","").replace("  "," ").replace("..",".").trim()}</p>
                             </Link>
-                            <p className="mt-2 text-base dark:text-white/70">Alcohol by Volume: {item.alcoholByVolume}</p>
-                            <p className="mt-2 text-base dark:text-white/70">Colour: {item.colour}</p>
-                            <p className="mt-2 text-base dark:text-white/70">Bitterness: {item.bitterness}</p>
-                            <p className="mt-2 text-base dark:text-white/70">Brewing Program: {item.brewingProgram}</p>
-                            <p className="mt-2 text-base dark:text-white/70">Approximate Brewing Time: {item.approximateBrewingTime}</p>
+                            <p className="mt-2 text-base dark:text-white/70">Average Rating: {item.primaryRating}/5 stars<br></br>Recommended by {item.recommended} people</p>
+                            <p className="mt-2 text-base dark:text-white/70">Alcohol by Volume: {item.alcoholByVolume}%<br></br>Colour: {item.colour}<br></br>Bitterness: {item.bitterness}</p>
+                            <p className="mt-2 text-base dark:text-white/70">Brewing Program: {item.brewingProgram}<br></br>Approximate Brewing Time: {item.approximateBrewingTime}</p>
                             {/* <p className="mt-2 text-base dark:text-white/70">Brew Notes: {item.brewNotes}</p> */}
                             <p className="mt-2 text-base dark:text-white/70">
-                                Inside The Box: {
+                                Inside The Box:<br></br>{
                                     [item.insideTheBox.elements, item.insideTheBox.enhancers, item.insideTheBox.hops, item.insideTheBox.yeasts, item.insideTheBox.primers]
                                         .filter(arr => arr.length)
                                         .map(arr => arr.join(", "))
                                         .join(", ")
-                                }
+                                }, Droid Cleanse
                             </p>
                         </div>
                         <div className="flex flex-col items-end">
-                            <p className="text-xl font-bold dark:text-white/90">Brewprint: ${item.price}</p>
-                            <p className="mt-2 text-base dark:text-white/70">Ingredients: ${item.totalPriceOfIngredients}</p>
-                            <p className="mt-2 text-sm dark:text-white/70">{item.price > item.totalPriceOfIngredients ? "Cheaper buying ingredients individually" : "Cheaper buying Brewprint"}</p>
+                            <p className="text-xl font-bold dark:text-white/90">Brewprint: ${item.price}<br></br>Ingredients: ${item.totalPriceOfIngredients}
+                            <span className="mt-2 text-sm dark:text-white/70">{item.price > item.totalPriceOfIngredients ? "Cheaper buying ingredients individually" : "Cheaper buying Brewprint"}</span></p>
                         </div>
                     </div>
                 ))}
